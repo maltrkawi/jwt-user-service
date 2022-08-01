a0c477b19b6bc010490dcb82eace35a677e3d251
Mohammad Altrkawi <m.altrkawi@gmail.com>
2022-08-01 20:27:03 +0300

    Custom Authorization Filter

--- src/main/java/com/maltrkawi/userservice/filter/CustomJwtDsl.java
+++ src/main/java/com/maltrkawi/userservice/filter/CustomJwtDsl.java
@@ -3,13 +3,17 @@ package com.maltrkawi.userservice.filter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 public class CustomJwtDsl extends AbstractHttpConfigurer<CustomJwtDsl, HttpSecurity> {
 
     @Override
     public void configure(HttpSecurity builder) throws Exception {
         AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
-        builder.addFilter(new CustomAuthenticationFilter(authenticationManager));
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager);
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        builder.addFilter(customAuthenticationFilter);
+        builder.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     public static CustomJwtDsl customJwtDsl(){
--- src/main/java/com/maltrkawi/userservice/security/SecurityConfig.java
+++ src/main/java/com/maltrkawi/userservice/security/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.maltrkawi.userservice.security;
 
-import com.maltrkawi.userservice.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static com.maltrkawi.userservice.filter.CustomJwtDsl.customJwtDsl;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -27,7 +26,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/api/login").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
         http.apply(customJwtDsl());
 
         return http.build();
